d902d27d73a815920fdf51780542480acfc9452f
Administrator <2287300106@qq.com>
2016-10-28 17:56:17 +0800

    集合测试例子

---
+++ src/com/org3/excise4/ListTest.java
@@ -0,0 +1,16 @@
+package com.org3.excise4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yiyp on 2016/10/28 0028.
+ */
+public class ListTest {
+
+    public List coursersToSelect;
+
+    public ListTest() {
+        this.coursersToSelect = new ArrayList();
+    }
+}
